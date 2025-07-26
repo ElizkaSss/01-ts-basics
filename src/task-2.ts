33bb91f3047309ee34ad8633e928878f2c72fa4f
Eliza <elizavetasmolka@gmail.com>
2025-07-26 10:58:47 +0200

    .

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,14 +1,14 @@
 // Задача 2. Інтерфейси
-interface product {
+interface Product {
     readonly id: number;
     title: string;
     description?: string;
 }
 
-const product = {
+const Product: Product = {
   id: 1,
   title: "Tablet",
   description: "Compact and fast",
 };
 
-console.log(`Product: ${JSON.stringify(product)}`);
+console.log(`Product: ${JSON.stringify(Product)}`);
